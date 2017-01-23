177a1b7b32b41c1ef066f0c67fb15a106888ddd5
sunyanguo <sunyanguo@lvmama.com>
2017-01-23 15:17:11 +0800

    Fit swift 3

--- HexClock/main.swift
+++ HexClock/main.swift
@@ -15,43 +15,42 @@ class HexClock : NSObject {
     override init() {
         super.init()
 
-        NSNotificationCenter.defaultCenter().addObserverForName(NSApplicationDidChangeScreenParametersNotification, object: NSApplication.sharedApplication(), queue: NSOperationQueue.mainQueue()) { (x) -> Void in
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.NSApplicationDidChangeScreenParameters, object: NSApplication.shared(), queue: OperationQueue.main) { (x) -> Void in
             self.updateScreens()
         }
         updateScreens()
     }
 
-	func reinterpret_decimal_as_hex( inDecimalThatIsReallyHex:Int ) -> Int {
-		var	highNybble : Int = inDecimalThatIsReallyHex / 10;
-		var	lowNybble : Int = inDecimalThatIsReallyHex - highNybble;
+	func reinterpret_decimal_as_hex( _ inDecimalThatIsReallyHex:Int ) -> Int {
+		let	highNybble : Int = inDecimalThatIsReallyHex / 10;
+		let	lowNybble : Int = inDecimalThatIsReallyHex - highNybble;
 		return highNybble * 16 + lowNybble;
 	}
 
     func tick() {
-        let now = NSDate()
+        let now = Date()
 
-        let cal = NSCalendar.currentCalendar()
-        let components = cal.components(.CalendarUnitHour | .CalendarUnitMinute | .CalendarUnitSecond, fromDate: now)
-        //NSLog("%02d:%02d:%02d", components.hour, components.minute, components.second)
+        let cal = Calendar.current
+        let components = cal.dateComponents([.hour, .minute, .second], from: now)
+        NSLog("%02d:%02d:%02d", components.hour!, components.minute!, components.second!)
         for window in windows
 		{
-            window.backgroundColor = NSColor(deviceRed: CGFloat(reinterpret_decimal_as_hex(components.hour))/255.0, green: CGFloat(reinterpret_decimal_as_hex(components.minute))/255.0, blue: CGFloat(reinterpret_decimal_as_hex(components.second)) / 255.0, alpha: 1.0)
+            window.backgroundColor = NSColor(deviceRed: CGFloat(reinterpret_decimal_as_hex(components.hour!))/255.0, green: CGFloat(reinterpret_decimal_as_hex(components.minute!))/255.0, blue: CGFloat(reinterpret_decimal_as_hex(components.second!)) / 255.0, alpha: 1.0)
 
             let timeIntervalSinceReferenceDate = now.timeIntervalSinceReferenceDate
-            let to : NSTimeInterval = 1 - (timeIntervalSinceReferenceDate - NSTimeInterval(Int(timeIntervalSinceReferenceDate)))
-            NSTimer.scheduledTimerWithTimeInterval(to, target: self, selector: "tick", userInfo: nil, repeats: false)
+            let to : TimeInterval = 1 - (timeIntervalSinceReferenceDate - TimeInterval(Int(timeIntervalSinceReferenceDate)))
+            Timer.scheduledTimer(timeInterval: to, target: self, selector: #selector(HexClock.tick), userInfo: nil, repeats: false)
         }
     }
 
     func updateScreens() {
-		var	oldWindows = windows;	// don't actually delete the old windows until we've created the new ones.
         windows.removeAll()
 
-        let screens = NSScreen.screens() as [NSScreen]
+        let screens = NSScreen.screens()! as [NSScreen]
         for screen in screens {
-            let window = NSWindow(contentRect: screen.frame, styleMask: NSBorderlessWindowMask, backing: .Buffered, defer: true)
-            window.level = Int(CGWindowLevelForKey(Int32(kCGDesktopWindowLevelKey)))
-            window.backgroundColor = NSColor.clearColor()
+            let window = NSWindow(contentRect: screen.frame, styleMask: NSBorderlessWindowMask, backing: .buffered, defer: true)
+            window.level = Int(CGWindowLevelForKey(CGWindowLevelKey(rawValue: 2)!))
+            window.backgroundColor = NSColor.clear
 			window.alphaValue = 0.7;
             window.orderFrontRegardless()
 
@@ -63,4 +62,4 @@ class HexClock : NSObject {
 }
 
 HexClock()
-NSApplication.sharedApplication().run()
+NSApplication.shared().run()
